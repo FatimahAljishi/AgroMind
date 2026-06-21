337f387fe53291eca0c34250b005109104014084
RawanDak <rawandaknan@gmail.com>
2026-06-21 13:21:11 +0300

    Fix CORS for EC2 deployment

--- backend/main.py
+++ backend/main.py
@@ -75,7 +75,9 @@ app = FastAPI(title="AgroMind API", version="3.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:5173",
-        "http://127.0.0.1:5173",],
+        "http://127.0.0.1:5173",
+        "http://98.80.119.7:5173",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
