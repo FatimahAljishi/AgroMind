9d834cd3971cbb78f2928ea0041c931e88646706
FATIMAH ALJISHI <tamit0o0o@hotmail.com>
2026-07-07 21:58:37 +0300

    Fix allowed origin URL format
    Removed trailing slash from allowed origin URL.

--- src/backend/main.py
+++ src/backend/main.py
@@ -77,7 +77,7 @@ app.add_middleware(
     allow_origins=[ "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://98.80.119.7:5173",
-        "https://agromind-by-fatimah.vercel.app/",
+        "https://agromind-by-fatimah.vercel.app",
         ],
     allow_credentials=True,
     allow_methods=["*"],
